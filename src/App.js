0c142209c0c06319898ea0c50a2b7435f8c9bb58
Filippo Biondi <filomail@gmail.com>
2021-01-15 23:02:00 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import { Route, Switch } from "react-router-dom";
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/products/:id" component={ProductDetails} />
+            <Route
+              path="/products"
+              render={(props) => <Products sortBy="newest" {...props} />}
+            />
+            <Route path="/posts" component={Posts} />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/" component={Home} />
+          </Switch>
+          {/* 
+          you can use "exact" or Switch (from the most specific 
+          to the most generic route)
+          */}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
