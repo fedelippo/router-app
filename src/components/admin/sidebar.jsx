e1c9ffa6a6d8efba73118ab9e07dbc4dc711cfbd
Filippo Biondi <filomail@gmail.com>
2021-01-19 22:08:41 +0000

    Add programmatic routing and nested routing

---
+++ src/components/admin/sidebar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+const SideBar = () => {
+  return (
+    <ul>
+      <li>
+        <Link to="/admin/posts">Posts</Link>
+        <Link to="/admin/users">Users</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default SideBar;
